917ced8e31893fcde51e5a975a69b37d19a70cce
sztobar <bszewczyk91@gmail.com>
2016-05-15 05:11:20 +0200

    added assets

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import GameState from './states/GameState';
 
+function preload() {
+  //game.load.image('img', 'assets/');
+  game.load.spritesheet('chars', 'assets/roguelikeChar_magenta.png', 16, 16);
+  game.load.spritesheet('dungeon', 'assets/roguelikeDungeon_magenta.png', 16, 16);
+  game.load.spritesheet('indoor', 'assets/roguelikeIndoor_magenta.png', 16, 16);
+  game.load.spritesheet('sheet', 'assets/roguelikeSheet_magenta.png', 16, 16);
+}
+
 class Game extends Phaser.Game {
 
   constructor() {
-    super(500, 500, Phaser.AUTO, 'content', null);
+    super(800, 600, Phaser.AUTO, '', {preload, create, update});
     this.state.add('GameState', GameState, false);
     this.state.start('GameState');
   }
 
 }
 
-new Game();
+const game = new Game();
